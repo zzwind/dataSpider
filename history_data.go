17e5523856201c929e04e125c4d8b13b591f9e4e
zzWinD <gongqizhou@qq.com>
2017-02-05 19:58:43 +0800

    完成: 1. 原始数据的初始化 2. td的数据获取,分页下一页的正则完成 将来： 历史数据入库 多线程版本

---
+++ history_data.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"regexp"
+
+	"time"
+
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const LIST_URL = "http://quotes.money.163.com/fund/jzzs_%s.html?start=%s&end=%s"
+
+func StartTdData() {
+
+	db, err := sql.Open("mysql", "root:123456@/fund")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rows, err := db.Query("SELECT id,symbol,sname,publishdate,lastupdate FROM fundmetadata WHERE publishdate!='2000-00-00' ORDER BY id ASC ")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Fetch rows
+	for rows.Next() {
+
+		row := new(FundList)
+		// get RawBytes from data
+		err = rows.Scan(&row.Id, &row.SYMBOL, &row.SNAME, &row.PUBLISHDATE, &row.LASTUPDATE)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		fmt.Println(row.Id, row.SYMBOL, row.SNAME, row.PUBLISHDATE, row.LASTUPDATE)
+
+		nextPage(Download(fmt.Sprintf(LIST_URL, row.SYMBOL, row.LASTUPDATE, time.Now().Format("2006-01-02"))))
+
+		os.Exit(0)
+
+	}
+	if err = rows.Err(); err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	//`http://quotes.money.163.com/fund/jzzs_163113.html?start=2016-09-01&end=2017-01-26`
+
+}
+
+func nextPage(b []byte) {
+
+	reg_next_page := regexp.MustCompile(`<a href="([^>]*?)" class="pages_flip">下一页</a>`) //正则表达式的惰性匹配是从头匹配到尾部，而不能从尾部匹配上来。
+
+	matchNP := reg_next_page.FindAllSubmatch(b, -1)
+
+	for _, j := range matchNP {
+		fmt.Printf("%s\n", j[1])
+	}
+
+}
+
+func getTdData(b []byte) {
+	reg1 := regexp.MustCompile(`<td>(?:<span class=".*?">)?(.*?)(?:</span>)?</td>`) //<td>.*?([\d\-\.]*).*?</td>        <td.*?>([\d\-\.]*).*?</td>
+
+	matchTd := reg1.FindAllSubmatch(b, -1)
+
+	for _, j := range matchTd {
+		fmt.Printf("%s\n", j[1])
+	}
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
 //this program for data spider
 //以沉浸的心态做好这件事，忘掉努力奋斗坚持。
 func main() {
-	fmt.Println("STW")
+	t := time.Now()
+
+	//要执行的任务1：元数据  2：历史数据
+	mission := 2
+
+	if mission == 1 {
+
+		//fmt.Println("STW")
+		//Download("http://quotes.money.163.com/fund/jzzs_163113.html?start=2015-12-01&end=2017-01-26")
+		GetMetaData("开放型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&query=STYPE:FDO&fields=no,PUBLISHDATE,SYMBOL,SNAME,NAV,PCHG,M12RETRUN,SLNAVG,LJFH,ZJZC,SGZT,OFPROFILE15&sort=PCHG&order=desc&count=6000&type=query")
+		GetMetaData("货币型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&page=0&query=TYPE1:MONEY&fields=no,PUBLISHDATE,SYMBOL,SNAME,CUR4,CURNAV_001,CURNAV_010,CURNAV_011,OFPROFILE8,OFPROFILE15&sort=CUR4&order=desc&count=600&type=query")
+		GetMetaData("保本型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&page=0&query=STYPE:FDO;TYPE3:BBX&fields=no,PUBLISHDATE,SYMBOL,SNAME,NAV,PCHG,M12RETRUN,SLNAVG,LJFH,ZJZC,SGZT,OFPROFILE15&sort=PCHG&order=desc&count=200&type=query")
+		GetMetaData("股票型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&page=0&query=STYPE:FDO;TYPE3:GPX&fields=no,PUBLISHDATE,SYMBOL,SNAME,NAV,PCHG,M12RETRUN,SLNAVG,LJFH,ZJZC,SGZT,OFPROFILE15&sort=PCHG&order=desc&count=300&type=query")
+		GetMetaData("指数型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&page=0&query=STYPE:FDO;TYPE3:ZSX&fields=no,PUBLISHDATE,SYMBOL,SNAME,NAV,PCHG,M12RETRUN,SLNAVG,LJFH,ZJZC,SGZT,OFPROFILE15&sort=PCHG&order=desc&count=900&type=query")
+		GetMetaData("混合型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&page=0&query=STYPE:FDO;TYPE3:HHX&fields=no,PUBLISHDATE,SYMBOL,SNAME,NAV,PCHG,M12RETRUN,SLNAVG,LJFH,ZJZC,SGZT,OFPROFILE15&sort=PCHG&order=desc&count=3000&type=query")
+		GetMetaData("债券型", "http://quotes.money.163.com/fn/service/netvalue.php?host=/fn/service/netvalue.php&page=0&query=STYPE:FDO;TYPE3:ZQX&fields=no,PUBLISHDATE,SYMBOL,SNAME,NAV,PCHG,M12RETRUN,SLNAVG,LJFH,ZJZC,SGZT,OFPROFILE15&sort=PCHG&order=desc&count=2000&type=query")
+
+	} else if mission == 2 {
+		StartTdData()
+	}
+
+	fmt.Println(time.Since(t).Seconds())
+}
+
+//<tbody>\n<tr>\n(<td>(.*)</td>\n){3}</tr>\n</tbody>
+//<tbody>[\s\S\n]*</tbody>
+
+func Download(url string) []byte {
+	r, err := http.Get(url)
+	defer r.Body.Close()
+	if err != nil {
+		panic("Download error " + url)
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic("Download error " + url)
+	}
+
+	return b
+
+	//ioutil.WriteFile("tbody.txt", b, os.ModePerm)
+
+	//b, err := ioutil.ReadFile("tbody.txt")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//t := time.Now()
+
+	//hmtx:=(string)b
+
+	//reg0 := regexp.MustCompile(`<tbody>[\s\S\n]*</tbody>`)
+
+	//matchTbody := reg0.Find(b)
+
+	//regTd := regexp.MustCompile(`<td>(?:<span class=".*?">)?(.*?)(?:</span>)?</td>`) //<td>.*?([\d\-\.]*).*?</td>        <td.*?>([\d\-\.]*).*?</td>
+	//matchTd := regTd.FindAllSubmatch(b, -1)
+	//
+	//for _, j := range matchTd {
+	//	fmt.Printf("%s\n", j[1])
+	//}
+
+	//fmt.Println(time.Since(t).Seconds())
+
 }
